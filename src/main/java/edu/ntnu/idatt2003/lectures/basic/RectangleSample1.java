0f2e29bc0c28425ac7f9983d20c29bad26df4b95
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2024-02-20 10:54:33 +0100

    removed ractangle example 1

--- src/main/java/edu/ntnu/idatt2003/lectures/basic/RectangleSample1.java
+++ src/main/java/edu/ntnu/idatt2003/lectures/basic/RectangleSample1.java
@@ -1,16 +1,22 @@
-//https://www.tutorialspoint.com/explain-javafx-scene-graph
 package edu.ntnu.idatt2003.lectures.basic;
 
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class RectangleSample1 extends Application {
     @Override
     public void start(Stage stage) {
-        Group group = new Group();
-        Scene scene = new Scene(group, 500, 500);
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500);
+        
+        Rectangle r = new Rectangle(25,25,250,250);
+        r.setFill(Color.BLUE);
+        root.getChildren().add(r);
+        
         stage.setTitle("JavaFX Scene Graph Demo");
         stage.setScene(scene);
         stage.show();
